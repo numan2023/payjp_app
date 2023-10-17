464d9dfe28c3321647cc309dd63e7035b97f8fe5
numan2023 <fullmetal0212@gmail.com>
2023-10-17 17:12:03 +0900

    カード登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   get 'users/show'
   devise_for :users
   root "items#index"
 
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_055245) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_064916) do
+  create_table "cards", charset: "utf8", force: :cascade do |t|
+    t.string "customer_token", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -31,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_17_055245) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
 end
